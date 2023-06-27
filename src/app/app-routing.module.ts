a54279f3690a28fb1f26f4ded123644ebc32176f
Davi Batista <davideosmar11@gmail.com>
2023-06-27 18:04:39 -0300

    refactor: recreated header, routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/pages/index/index.component';
 import { AboutComponent } from './components/pages/about/about.component';
-import { SkillsComponent } from './components/pages/skills/skills.component';
 import { ProjectsComponent } from './components/pages/projects/projects.component';
 import { TalkToMeComponent } from './components/pages/talk-to-me/talk-to-me.component';
+import { ServicesComponent } from './components/pages/services/services.component';
 
 
 const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'skills', component: SkillsComponent},
+  {path: 'services', component: ServicesComponent},
   {path: 'projects', component: ProjectsComponent},
   {path: 'talk+to+me', component: TalkToMeComponent}
 ];
