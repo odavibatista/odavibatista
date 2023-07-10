c74116a4dbfdfb701f12b72c3dc8e8197800df19
Davi Batista <davideosmar11@gmail.com>
2023-07-10 12:09:13 -0300

    refactor: organized projects in its stacks

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -19,17 +19,6 @@ export class ProjectsComponent {
     },
 
     {
-      imgUrl: "../../../../assets/images/projects/e-commerce-sql-slide.jpg",
-      title: "SQL E-Commerce",
-      description: "Banco de dados relacional montado para simular um sistema de back-end de um e-commerce de artigos de informática através de Sequelize, em conjunto com seus endpoints e sua API modelados.",
-      deployUrl: "",
-      ghUrl: "https://github.com/daviebatista/e-commerce-sequelize",
-      figmaUrl: "",
-      docs: '',
-      dbUrl: 'https://user-images.githubusercontent.com/91736880/232808581-732ae6ce-8478-4e33-9a5a-3b0f04592024.png'
-    },
-
-    {
       imgUrl: "../../../../../assets/images/projects/old-david-slide.jpg",
       title: "Old David's Shop",
       description: "E-commerce construído através de React, Vite e Bootstrap, com funções de cadastro, carrinho de compras, e finalização de compras com endereço e recibo, com layout e wireframe próprios.",
@@ -37,17 +26,6 @@ export class ProjectsComponent {
       ghUrl: "https://github.com/daviebatista/vivi-bot",
       figmaUrl: "",
       docs: 'https://github.com/daviebatista/vivi-bot/tree/V2.3/commands#readme',
-      dbUrl: ''
-    },
-    
-    {
-      imgUrl: "../../../../../assets/images/projects/vivi-bot-slide.jpg",
-      title: "Vivi Bot",
-      description: "Projeto de bot de Discord open-source, feito com NodeJs e TypeScript pela biblioteca DiscordJS. Possui funções nativas para as jogadas de dados, e a aplicação pode ser clonada.",
-      deployUrl: "https://daviebatista.github.io/beverage-ecommerce/",
-      ghUrl: "https://github.com/daviebatista/beverage-ecommerce",
-      figmaUrl: "https://www.figma.com/file/FLaRgeOP2ZW6nzsXYzdVmS/Old-David's-Shop?type=design&node-id=101-2&mode=design&t=ZoSvhISzfrVSdvr0-0",
-      docs: '',
       dbUrl: ''
     },
 
@@ -58,6 +36,17 @@ export class ProjectsComponent {
       deployUrl: "https://daviebatista.github.io/exalted-diceroll",
       ghUrl: "https://github.com/daviebatista/exalted-diceroll",
       figmaUrl: '',
+      docs: '',
+      dbUrl: ''
+    },
+
+    {
+      imgUrl: "../../../../../assets/images/projects/onebitmusic-slide.jpg",
+      title: "OneBitMusic",
+      description: "Landing Page construída como desafio do primeiro hackathon da OneBitCode, com a proposta de se seguir o wireframe do Figma, e implementando funções à página.",
+      deployUrl: "https://onebitmusic.vercel.app/",
+      ghUrl: "https://github.com/daviebatista/onebitcode-hackathon-1",
+      figmaUrl: "https://www.figma.com/file/qV4ADMl4Vvytj15d2aBEMH/Onebitmusic?type=design&node-id=1674-135&mode=design",
       docs: '',
       dbUrl: ''
     },
@@ -74,23 +63,45 @@ export class ProjectsComponent {
     },
 
     {
+      imgUrl: "../../../../../assets/images/projects/portfolio-slide.jpg",
+      title: "Dev Portfolio",
+      description: "Single Page Application produzida com Angular 16, Bootstrap e SCSS, com wireframe desenhado no Figma para a criação e estilização do projeto e suas funcionalidades.",
+      deployUrl: "",
+      ghUrl: "https://github.com/daviebatista/daviebatista",
+      figmaUrl: "https://www.figma.com/file/gnttWmAfu7TyKYY1WHzHGw/Portfolio---V2?type=design&node-id=3-14&mode=design&t=hA68nkqzdyHtvUgB-0",
+      docs: '',
+      dbUrl: ''
+    },
+
+    {
+      imgUrl: "../../../../assets/images/projects/e-commerce-sql-slide.jpg",
+      title: "SQL E-Commerce",
+      description: "Banco de dados relacional montado para simular um sistema de back-end de um e-commerce de artigos de informática através de Sequelize, em conjunto com seus endpoints e sua API modelados.",
+      deployUrl: "",
+      ghUrl: "https://github.com/daviebatista/e-commerce-sequelize",
+      figmaUrl: "",
+      docs: '',
+      dbUrl: 'https://user-images.githubusercontent.com/91736880/232808581-732ae6ce-8478-4e33-9a5a-3b0f04592024.png'
+    },
+    
+    {
+      imgUrl: "../../../../../assets/images/projects/vivi-bot-slide.jpg",
+      title: "Vivi Bot",
+      description: "Projeto de bot de Discord open-source, feito com NodeJs e TypeScript pela biblioteca DiscordJS. Possui funções nativas para as jogadas de dados, e a aplicação pode ser clonada.",
+      deployUrl: "https://daviebatista.github.io/beverage-ecommerce/",
+      ghUrl: "https://github.com/daviebatista/beverage-ecommerce",
+      figmaUrl: "https://www.figma.com/file/FLaRgeOP2ZW6nzsXYzdVmS/Old-David's-Shop?type=design&node-id=101-2&mode=design&t=ZoSvhISzfrVSdvr0-0",
+      docs: '',
+      dbUrl: ''
+    },
+
+    {
       imgUrl: "../../../../../assets/images/projects/veritas-back-end-slide.jpg",
       title: "Project Veritas Back-End",
       description: "Banco de dados relacional + API para um serviço de streaming de aulas de ciências humanas, com categorias, upload de vídeos e interface de administrador.",
       deployUrl: "",
       ghUrl: "https://github.com/daviebatista/project-veritas-backend",
       figmaUrl: "",
-      docs: '',
-      dbUrl: ''
-    },
-
-    {
-      imgUrl: "../../../../../assets/images/projects/onebitmusic-slide.jpg",
-      title: "OneBitMusic",
-      description: "Landing Page construída como desafio do primeiro hackathon da OneBitCode, com a proposta de se seguir o wireframe do Figma, e implementando funções à página.",
-      deployUrl: "https://onebitmusic.vercel.app/",
-      ghUrl: "https://github.com/daviebatista/onebitcode-hackathon-1",
-      figmaUrl: "https://www.figma.com/file/qV4ADMl4Vvytj15d2aBEMH/Onebitmusic?type=design&node-id=1674-135&mode=design",
       docs: '',
       dbUrl: ''
     },
@@ -106,16 +117,7 @@ export class ProjectsComponent {
       dbUrl: ''
     },
 
-    {
-      imgUrl: "../../../../../assets/images/projects/portfolio-slide.jpg",
-      title: "Dev Portfolio",
-      description: "Single Page Application produzida com Angular 16, Bootstrap e SCSS, com wireframe desenhado no Figma para a criação e estilização do projeto e suas funcionalidades.",
-      deployUrl: "",
-      ghUrl: "https://github.com/daviebatista/daviebatista",
-      figmaUrl: "https://www.figma.com/file/gnttWmAfu7TyKYY1WHzHGw/Portfolio---V2?type=design&node-id=3-14&mode=design&t=hA68nkqzdyHtvUgB-0",
-      docs: '',
-      dbUrl: ''
-    },
+
   ]
 
 
