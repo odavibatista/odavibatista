9aefd61541094d7ae87c015b77b893eb028ea6b8
Davi Batista <davideosmar11@gmail.com>
2025-04-21 16:10:20 -0300

    feat: added new experiences

--- src/app/components/pages/about/experiences/experiences.component.ts
+++ src/app/components/pages/about/experiences/experiences.component.ts
@@ -8,9 +8,15 @@ import { Component } from '@angular/core';
 export class ExperiencesComponent {
   readonly Roles = [
     {
+      imgUrl: "https://cdn-icons-png.flaticon.com/512/5969/5969474.png",
+      roleName: "Criação de Sistema - Empresa Confidencial",
+      time: "02/2024 - Atualmente",
+      description: "Desenvolvimento de um sistema sob medida completo para uma empresa atuante no setor de fotografia, design gráfico, impressões e criação de fotoprodutos. O projeto consiste em um sistema para controle de entrada e saída de serviços, insumos e atividades realizadas por cada funcionário da empresa, de modo a centralizar o controle de vendas de suas múltiplas lojas."
+    },
+    {
       imgUrl: "/assets/images/corabyte-logo.png",
-      roleName: "Desenvolvedor Full-Stack",
-      time: "08/2024 - Atualmente",
+      roleName: "Desenvolvedor Full-Stack Júnior",
+      time: "08/2024 - 11/2024",
       description: "Planejamento, arquitetura e engenharia de sistemas, desenvolvimento back-end e front-end, documentação de projetos, desenvolvimento de testes unitários, desenvolvimento do escopo de aplicações."
     },
     {
